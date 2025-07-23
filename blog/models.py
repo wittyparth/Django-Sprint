1208d4ca3ba89fc594db1f898644e491f6257222
Partha Saradhi <parthasaradhimunakala@gmail.com>
2025-07-23 12:55:01 +0530

    fixing the mistake i made by pushing previous code

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,17 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils.text import slugify
-from django.utils import timezone
 from django.core.validators import FileExtensionValidator
-
+import math
+from django.conf import settings
 # Create your models here.
+# models.py
+from django.contrib.auth.models import AbstractUser
+from cloudinary.models import CloudinaryField
 
+class User(AbstractUser):
+    is_active = models.BooleanField(default=False)  # User inactive until email verified
+    pass
 
 class Tag(models.Model):
     name = models.CharField(max_length=30, unique=True)
@@ -14,26 +19,32 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 
 class BlogPost(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=3000)  # title can be very long
-    slug = models.SlugField(unique=True, blank=True)
     content = models.TextField()
-    image = models.ImageField(
-        upload_to='blog_images/',
-        null=True,
-        blank=True,
-        validators=[FileExtensionValidator(['jpg', 'jpeg', 'png', 'webp'])]
-    )
+    image = CloudinaryField('image', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published')
     )
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     publish_date = models.DateTimeField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    tags = models.ManyToManyField(Tag, related_name='posts', blank=True) # RElated name will give a custom to access posts from tag like tag = Tag.object.get(name="js") tag.posts.all()
+    reading_time = models.PositiveIntegerField(default=1)  # minutes
+
 
     def is_published(self):
         return self.status == 'published' and (self.publish_date is None or self.publish_date <= timezone.now())
@@ -43,33 +54,29 @@ class BlogPost(models.Model):
             base_slug = slugify(self.title)[:50]
             unique_suffix = str(uuid.uuid4())[:8]
             self.slug = f"{base_slug}-{unique_suffix}"
+        if self.content:
+            word_count = len(self.content.split())
+            self.reading_time = math.ceil(word_count / 200)
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
     
 class Comment(models.Model):
-    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    parent = models.ForeignKey(
-        'self', null=True, blank=True,
-        related_name='replies',
-        on_delete=models.CASCADE
-    )
+    post = models.ForeignKey('BlogPost', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def is_reply(self):
-        return self.parent is not None
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
 
     class Meta:
-        ordering = ['-created_at']
-
-    def __str__(self):
-        return f"Comment by {self.author.username} on {self.post.title}"
+        ordering = ['created_at']
     
 class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='likes')
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -78,12 +85,9 @@ class Like(models.Model):
 
     def __str__(self):
         return f"{self.user.username} likes {self.post.title}"
-    
-from django.db import models
-from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
     avatar_url = models.URLField(blank=True, null=True)
     bio = models.TextField(blank=True)
     website = models.URLField(blank=True, null=True)
@@ -92,22 +96,20 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
-        
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         from .models import Profile  # avoid circular imports
         Profile.objects.create(user=instance)
 
-from django.db import models
-from django.contrib.auth.models import User
+
 
 class Bookmark(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     post = models.ForeignKey('BlogPost', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -116,3 +118,22 @@ class Bookmark(models.Model):
 
     def __str__(self):
         return f"{self.user.username} bookmarked {self.post.title}"
+
+class CommentLike(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    comment = models.ForeignKey('Comment', on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'comment')
+
+    def __str__(self):
+        return f"{self.user.username} liked comment {self.comment.id}"
+
+class PostView(models.Model):
+    post = models.ForeignKey(BlogPost,related_name="views",on_delete=models.CASCADE)
+    ip_address = models.GenericIPAddressField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.ip_address} viewed {self.post.title}"
